3d497f37e9a9ad0fb2e34733b489ea5eae6383f4
vittoema96 <vitto.e1996@live.it>
2023-05-25 11:32:50 +0200

    Exe in one file. Zip version.
    Removed dist/IFC/ folder.
    build_exe.py now builds a single file instead of a folder with an .exe and a lot of other files. It also creates a zip version of the exe.
    IFC.exe is git ignored.

--- build_exe.py
+++ build_exe.py
@@ -1,12 +1,13 @@
 import os
-import sys
+from zipfile import ZipFile
 
 if __name__ == '__main__':
-    os.system('pyinstaller --noconfirm --onedir --windowed '
+    os.system('pyinstaller --noconfirm --onefile --windowed '
               '--icon "./ifc/resources/icon.ico" '
               '--name "IFC" '
               '--add-data "./ifc/data;ifc/data/" '
               '--add-data "./ifc/gui;ifc/gui/" '
               '--add-data "./ifc/resources;ifc/resources/"  '
               '"./ifc/main.py"')
-    
+    with ZipFile(os.path.join('dist', 'IFC.zip'), mode="w") as archive:
+        archive.write(os.path.join('dist', 'IFC.exe'))
